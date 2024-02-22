8026da58b34723c9f113a554afe8d41df571f847
Silitonix <silitonix@gmail.com>
2024-02-22 16:42:35 +0330

    created space for code

---
+++ source/toekenarea.ts
@@ -0,0 +1,25 @@
+class TokenArea
+{
+  text: string[];
+
+  col: number = 0;
+  row: number = 0;
+
+  write(character: string)
+  {
+    if (character == '\n')
+    {
+      this.text.push("");
+      return;
+    }
+    let line = this.text[this.row];
+    line = `${line?.slice(0,this.col)}${character}${line?.slice(0,this.col)}`;
+    this.text[this.row] = line;
+  }
+
+  constructor (text: string)
+  {
+    const lines = text.split("\n");
+    this.text = lines;
+  }
+}
